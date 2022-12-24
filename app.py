bb3ef2a18ba30d74858843ab0b7c724c63370aff
romainrey <rom.rey06@gmail.com>
2022-12-24 11:38:10 -0800

    Fixing timezone init

--- app.py
+++ app.py
@@ -11,19 +11,22 @@ st.title('Calendar C-F-R')
 min_date, max_date = datetime.date.today(), datetime.date.today()+datetime.timedelta(days = 13)
 dates = st.date_input(label = 'Date Range', value = (datetime.date.today(), datetime.date.today()+datetime.timedelta(days = 7)),
 min_value=min_date, max_value = max_date)
-if len(dates)==2:
-    d0 = min(dates[0], max_date-datetime.timedelta(days = 1))
-    d1 = max(dates[1], d0+datetime.timedelta(days = 1))
-    dates = (d0, d1)
 
-    timezone = st_javascript("""await (async () => {
+timezone = st_javascript("""await (async () => {
                 const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
                 console.log(userTimezone)
                 return userTimezone
     })().then(returnValue => returnValue)""")
 
-    df = get_all_calendars(dates[0], dates[1], timezone).copy(deep = True)
-    pdf = get_styled_pivot_calendar(df)
+if len(dates)==2:
+    d0 = min(dates[0], max_date-datetime.timedelta(days = 1))
+    d1 = max(dates[1], d0+datetime.timedelta(days = 1))
+    dates = (d0, d1)
 
-    st.text("Showing for timezone: "+timezone)
-    st.table(pdf)
+    # Waiting for timezone to being loaded
+    if len(timezone)>2:
+        df = get_all_calendars(dates[0], dates[1], timezone).copy(deep = True)
+        pdf = get_styled_pivot_calendar(df)
+
+        st.text("Showing for timezone: "+timezone)
+        st.table(pdf)
